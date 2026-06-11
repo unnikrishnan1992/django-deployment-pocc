9610ec70666cd98ede2a0dfb2f2b0e0ab7e063cb
unnikrishnannair <unnikrishnannair@qburst.com>
2026-06-11 19:19:11 +0530

    Minor updates

--- items/views.py
+++ items/views.py
@@ -4,6 +4,8 @@ from django.http import JsonResponse
 def item_list(request):
     data = [
         {"id": 1, "name": "Laptop"},
-        {"id": 2, "name": "Mobile"},
+        {"id": 2, "name": "Laptop"},
+        {"id": 3, "name": "Mobile"},
+        {"id": 4, "name": "Mobile"},
     ]
     return JsonResponse(data, safe=False)
